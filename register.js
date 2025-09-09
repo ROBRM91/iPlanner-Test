9af98ac35813eb47581103e4d2a7aff477574678
ROBRM91 <ramsel.medina1825@gmail.com>
2025-09-09 11:55:28 -0600

    register.js

--- register.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            console.log('Service Worker registrado con éxito:', registration.scope);
-        }, function(err) {
-            console.log('Fallo el registro del Service Worker:', err);
-        });
-    });
-}
